9a85d7e72c086a27c6dc91371de44db1a5b7b20b
Artit Kiuwilai <first087@gmail.com>
2015-05-28 14:55:47 +0700

    Handle View

--- app/src/main/java/com/artitk/okhttpexample/ResultActivity.java
+++ app/src/main/java/com/artitk/okhttpexample/ResultActivity.java
@@ -3,11 +3,13 @@ package com.artitk.okhttpexample;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
-public class ResultActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     private RadioButton radioHeader;
     private RadioButton radioBody;
@@ -30,11 +32,33 @@ public class ResultActivity extends AppCompatActivity {
 
         setTitle(menuTitle);
 
+        radioHeader.setOnCheckedChangeListener(this);
+        radioBody.setOnCheckedChangeListener(this);
+
         switch (menuIndex) {
             case 0: callSyncGet();      break;  // Synchronous Get
             case 1: callASyncGet();     break;  // Asynchronous Get
             // TODO : Add other case
         }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (!isChecked) return;
+
+        String result = null;
+
+        int viewId = buttonView.getId();
+        switch (viewId) {
+            case R.id.radioHeader:
+                result = "Result from Header";  // TODO : Implement get store result
+                break;
+            case R.id.radioBody:
+                result = "Result from Body";    // TODO : Implement get store result
+                break;
+        }
+
+        textResult.setText(result);
     }
 
     private void callSyncGet() {
@@ -43,6 +67,23 @@ public class ResultActivity extends AppCompatActivity {
 
     private void callASyncGet() {
         // TODO : Implement Async Get
+    }
+
+    private void resetView() {
+        radioHeader.setEnabled(false);
+        radioHeader.setChecked(true);
+        radioBody.setEnabled(false);
+        progressBar.setVisibility(View.VISIBLE);
+        textResult.setVisibility(View.GONE);
+        textResult.setText("");
+    }
+
+    private void showView(String result) {
+        radioHeader.setEnabled(true);
+        radioBody.setEnabled(true);
+        progressBar.setVisibility(View.GONE);
+        textResult.setVisibility(View.VISIBLE);
+        textResult.setText(result);
     }
 
 }
